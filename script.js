2bd0bb1e6327a38b09153120a8e00b4f988f5b9f
yuanyutao1997 <typroduction@foxmail.com>
2023-04-11 00:37:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,21 +14,33 @@ document.getElementById("language-switch").addEventListener("click", function ()
 
 document.addEventListener("DOMContentLoaded", function () {
     const languageSwitch = document.getElementById("language-switch");
+    const languageSwitch2 = document.getElementById("language-switch-2"); // 添加第二个切换按钮的引用
     const chineseText = document.querySelectorAll(".chinese");
     const englishText = document.querySelectorAll(".english");
     let language = "chinese";
-    
+
     languageSwitch.addEventListener("click", function () {
-      if (language === "chinese") {
-        chineseText.forEach(text => text.style.display = "none");
-        englishText.forEach(text => text.style.display = "block");
-        language = "english";
-      } else if (language === "english") {
-        chineseText.forEach(text => text.style.display = "block");
-        englishText.forEach(text => text.style.display = "none");
-        language = "chinese";
-      }
+        if (language === "chinese") {
+            chineseText.forEach(text => text.style.display = "none");
+            englishText.forEach(text => text.style.display = "block");
+            language = "english";
+        } else if (language === "english") {
+            chineseText.forEach(text => text.style.display = "block");
+            englishText.forEach(text => text.style.display = "none");
+            language = "chinese";
+        }
     });
-  });
-  
-  
+
+    // 添加第二个切换按钮的事件监听器
+    languageSwitch2.addEventListener("click", function () {
+        if (language === "chinese") {
+            chineseText.forEach(text => text.style.display = "none");
+            englishText.forEach(text => text.style.display = "block");
+            language = "english";
+        } else if (language === "english") {
+            chineseText.forEach(text => text.style.display = "block");
+            englishText.forEach(text => text.style.display = "none");
+            language = "chinese";
+        }
+    });
+});
