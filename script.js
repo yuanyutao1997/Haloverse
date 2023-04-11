73586562cd76d05ab07551748e73f918ddc3f5ab
yuanyutao1997 <typroduction@foxmail.com>
2023-04-11 12:48:43 +0800

    把内容分配到页面+中英文切换

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-// 在这里添加您的 JavaScript 代码以实现交互效果
 document.getElementById("language-switch").addEventListener("click", function () {
-    const chineseContent = document.querySelectorAll(".chinese");
-    const englishContent = document.querySelectorAll(".english");
+  const chineseContent = document.querySelectorAll(".chinese");
+  const englishContent = document.querySelectorAll(".english");
 
-    chineseContent.forEach(element => {
-        element.style.display = element.style.display === "none" ? "" : "none";
-    });
+  chineseContent.forEach(element => {
+      element.style.display = element.style.display === "none" ? "" : "none";
+  });
 
-    englishContent.forEach(element => {
-        element.style.display = element.style.display === "none" ? "" : "none";
-    });
+  englishContent.forEach(element => {
+      element.style.display = element.style.display === "none" ? "" : "none";
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-    const languageSwitch = document.getElementById("language-switch");
-    const chineseText = document.querySelectorAll(".chinese");
-    const englishText = document.querySelectorAll(".english");
-    let language = "chinese";
-  
-    languageSwitch.addEventListener("click", function() {
-      if (language === "chinese") {
-        chineseText.forEach(text => text.style.display = "none");
-        englishText.forEach(text => text.style.display = "block");
-        language = "english";
-      } else {
-        chineseText.forEach(text => text.style.display = "block");
-        englishText.forEach(text => text.style.display = "none");
-        language = "chinese";
-      }
-    });
+  const languageSwitch = document.getElementById("language-switch");
+  const chineseText = document.querySelectorAll(".chinese");
+  const englishText = document.querySelectorAll(".english");
+  let language = "chinese";
+
+  languageSwitch.addEventListener("click", function() {
+    if (language === "chinese") {
+      chineseText.forEach(text => text.style.display = "none");
+      englishText.forEach(text => text.style.display = "block");
+      language = "english";
+    } else {
+      chineseText.forEach(text => text.style.display = "block");
+      englishText.forEach(text => text.style.display = "none");
+      language = "chinese";
+    }
   });
-  
+});
