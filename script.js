5693135d0071101ec8cff72a8efaef82d44e11a7
yuanyutao1997 <typroduction@foxmail.com>
2023-04-11 13:01:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,22 +10,24 @@ document.getElementById("language-switch").addEventListener("click", function ()
       element.style.display = element.style.display === "none" ? "" : "none";
   });
 });
-
 document.addEventListener("DOMContentLoaded", function() {
   const languageSwitch = document.getElementById("language-switch");
   const chineseText = document.querySelectorAll(".chinese");
   const englishText = document.querySelectorAll(".english");
   let language = "chinese";
 
+  // 隐藏英文内容
+  englishText.forEach(text => text.style.display = "none");
+
   languageSwitch.addEventListener("click", function() {
-    if (language === "chinese") {
-      chineseText.forEach(text => text.style.display = "none");
-      englishText.forEach(text => text.style.display = "block");
-      language = "english";
-    } else {
-      chineseText.forEach(text => text.style.display = "block");
-      englishText.forEach(text => text.style.display = "none");
-      language = "chinese";
-    }
+      if (language === "chinese") {
+          chineseText.forEach(text => text.style.display = "none");
+          englishText.forEach(text => text.style.display = "block");
+          language = "english";
+      } else {
+          chineseText.forEach(text => text.style.display = "block");
+          englishText.forEach(text => text.style.display = "none");
+          language = "chinese";
+      }
   });
 });
